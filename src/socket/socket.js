748abc244f0ca1fe5efaf18715db41c61a7ba4d0
bheemuk123 <bheemuk123@gmail.com>
2024-08-28 22:22:07 +0530

    last seen update

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,75 +1,81 @@
-const {Server} = require('socket.io');
-const { sendMessage, markAsRead } = require('../controllers/user.controller');
+const { Server } = require("socket.io");
+const { sendMessage, markAsRead } = require("../controllers/user.controller");
+const client = require("../db/connect/connections");
 
 // Initialize Socket.IO with CORS settings
- let   io ;
- let onlineUsers = []
- const userSocketMap = {}; // Store user ID to socket ID mapping
- function socketInit(httpServer){
-   io =  new Server(httpServer, {
-        cors: {
-          origin: "*", // Replace with your Angular app URL
-          methods: ["GET", "POST"],
-          credentials: true,
-        },
-      });
-      // Handle socket connections
-      
+let io;
+let onlineUsers = [];
+const userSocketMap = {}; // Store user ID to socket ID mapping
+function socketInit(httpServer) {
+  io = new Server(httpServer, {
+    cors: {
+      origin: "*", // Replace with your Angular app URL
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+  // Handle socket connections
 
-      io.on("connection", (socket) => {
-          let userId = socket.handshake.query.id;
-          socket.userId = userId;
-      
-          // Map the user ID to the socket ID
-          userSocketMap[userId] = socket.id;
-          console.log("New socket connection:", socket.userId);
-      
-          onlineUsers.push(userId);
-          io.emit('onlineusers', onlineUsers);
-      
-          socket.on("sendMessage", async (data) => {
-              socket.join(data.inbox_id);
-      
-              // Debugging logs
-              // console.log('Sender ID:', data.sender_id);
-              // console.log('Receiver ID:', data.receiver_id);
-      
-              let messages = await sendMessage(data);
-      
-              // Send confirmation to the sender
-      
-              // Fetch the receiver's socket ID using userSocketMap
-              const receiverSocketId = userSocketMap[data.receiver_id];
-      
-              // Send message received notification to the receiver
-              if (receiverSocketId) {
-                  io.to(receiverSocketId).emit("messageReceviced", messages);
-              } else {
-                  console.log('Receiver not connected');
-              }
-              socket.emit("sent", messages);
-          });
+  io.on("connection", (socket) => {
+    let userId = socket.handshake.query.id;
+    socket.userId = userId;
 
-          socket.on("read", async (data)=>{
-          let res = await markAsRead(data);
-          socket.emit('onMsgRead')
-          })
-      
-          socket.on("disconnect", () => {
-              let index = onlineUsers.indexOf(userId);
-              onlineUsers.splice(index, 1);
-              // socket.broadcast.emit('onlineusers', onlineUsers);
-              io.emit('onlineusers', onlineUsers);
-              console.log("Socket disconnected:", userId);
-      
-              // Remove from userSocketMap
-              delete userSocketMap[userId];
-          });
-      });
-      
-    
+    // Map the user ID to the socket ID
+    userSocketMap[userId] = socket.id;
+    console.log("New socket connection:", socket.userId);
+
+    onlineUsers.push(userId);
+    io.emit("onlineusers", onlineUsers);
+
+    socket.on("sendMessage", async (data) => {
+      socket.join(data.inbox_id);
+
+      // Debugging logs
+      // console.log('Sender ID:', data.sender_id);
+      // console.log('Receiver ID:', data.receiver_id);
+
+      let messages = await sendMessage(data);
+
+      // Send confirmation to the sender
+
+      // Fetch the receiver's socket ID using userSocketMap
+      const receiverSocketId = userSocketMap[data.receiver_id];
+
+      // Send message received notification to the receiver
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit("messageReceviced", messages);
+      } else {
+        console.log("Receiver not connected");
+      }
+      socket.emit("sent", messages);
+    });
+
+    socket.on("read", async (data) => {
+      let res = await markAsRead(data);
+      socket.emit("onMsgRead");
+    });
+
+    socket.on("disconnect", async () => {
+     try {
+      let index = onlineUsers.indexOf(userId);
+      onlineUsers.splice(index, 1);
+      // socket.broadcast.emit('onlineusers', onlineUsers);
+      io.emit("onlineusers", onlineUsers);
+      console.log("Socket disconnected:", userId);
+
+      let query = `UPDATE users
+  SET last_seen = CURRENT_TIMESTAMP
+  WHERE user_id = $1;`;
+
+  await client.query(query, [userId])
+      // Remove from userSocketMap
+      delete userSocketMap[userId];
+     } catch (error) {
+      // throw Error('Internal Server Error')
+      // console.log(error)
+     }
+    });
+  });
 }
-  
-  
 
-  module.exports = {socketInit}
+module.exports = { socketInit };
