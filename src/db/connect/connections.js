9762698d12f54ce6a84150a703b78816906c5ff2
bheemuk123 <bheemuk123@gmail.com>
2024-09-25 00:04:36 +0530

    keeping alive

--- src/db/connect/connections.js
+++ src/db/connect/connections.js
@@ -7,9 +7,18 @@ client.connect(function(err) {
 try {
   if (err) throw err;
   console.log("pl/sql DB Connected!");
+  keepDBActive()
 } catch (error) {
   console.log("DB connection closed...")
 }
 });
+
+ function keepDBActive(){
+ setInterval(async()=>{
+  let query = 'select * from dummytable;'
+  await client.query(query);
+  console.log('keeping alive..')
+ }, (4 * 60 * 1000))
+}
 
 module.exports = client;
